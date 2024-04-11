150d640b90d0f4243494d042487fe8180a0f459a
Sophie Woodcock <szoph101@hotmail.co.uk>
2024-04-11 12:11:25 +0100

    fixed contact page

--- components/ContactComponents/ContactPage.jsx
+++ components/ContactComponents/ContactPage.jsx
@@ -3,7 +3,7 @@ import SideBar from "../SideBar"
 
 const ContactPage = () => {
   return (
-    <div className="w-full h-full p-24 md:pl-44 lg:pl-44 bg-[#f5ebe0] dark:bg-medium dark:text-[#96A9C7] text-[#B08C70]">
+    <div className="w-full md:h-screen lg:h-screen h-full p-24 md:pl-44 lg:pl-44 bg-[#f5ebe0] dark:bg-medium dark:text-[#96A9C7] text-[#B08C70]">
         <SideBar />
         <h2 className="md:text-9xl lg:text-9xl text-4xl bring-up font-semibold">Contact</h2>
         <h3 className="md:text-4xl lg:text-4xl text-2xl">Get in touch</h3>
@@ -11,9 +11,8 @@ const ContactPage = () => {
         <form className="flex flex-col gap-6 pt-10 md:w-2/5 lg:w-2/5 w-full" action="mailto:szoph101@hotmail.co.uk" method="post" enctype="text/plain">
             <input type="text" name="name" className="opacity-75 focus:opacity-100 rounded-md p-2 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium placeholder-[#f5ebe0] dark:placeholder-medium focus:outline-none" 
             placeholder="Your name..."></input>
-            <input type="email" name="email" className="opacity-75 focus:opacity-100 rounded-md p-2 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium placeholder-[#f5ebe0] dark:placeholder-medium focus:outline-none" placeholder="Your email..."></input>
-            <textarea placeholder="Your message..." className="opacity-75 focus:opacity-100 rounded-md p-2 h-28 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium placeholder-[#f5ebe0] dark:placeholder-medium focus:outline-none"></textarea>
-            <button className="flex self-end px-4 text-center py-2 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium rounded-md hover:dark:bg-[#C6D6EC] hover:bg-[#7E4F38]">Send</button>
+            <textarea name="message" placeholder="Your message..." className="opacity-75 focus:opacity-100 rounded-md p-2 h-28 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium placeholder-[#f5ebe0] dark:placeholder-medium focus:outline-none"></textarea>
+            <button type="submit" className="flex self-end px-4 text-center py-2 dark:bg-[#96A9C7] bg-[#B08C70] text-[#f5ebe0] dark:text-medium rounded-md hover:dark:bg-[#C6D6EC] hover:bg-[#7E4F38]">Send</button>
         </form>
        <div className="bring-left">
        <h3 className="md:text-4xl lg:text-4xl text-2xl text-right">Email me directly at <span className="text-[#7E4F38] dark:text-[#C6D6EC]">szoph101@hotmail.co.uk</span></h3>
